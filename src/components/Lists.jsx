e6dfa99807af1040a85dce7b08f19544220f96c4
Ayush <thapaaayush115@gmail.com>
2022-11-04 23:41:08 +0545

    task with multiple fields added

--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 import '../css/lists.css';
 
 import {FaPenSquare} from 'react-icons/fa';
 import {AiTwotoneDelete} from 'react-icons/ai';
+import {ImCheckboxUnchecked} from 'react-icons/im';
+import {ImCheckboxChecked} from 'react-icons/im'
 
 export default function Lists(props) {
-const {todolists,setTodolists,showupdate,setShowupdate,setUtask,setUpdateid}=props;
+const {todolists,setTodolists,setShowupdatebutton,setUpdateindex,setTask,
+    setDescription,setCategory,categories,category}=props;
+
+
+ const [filter,setFilter]=useState('all');
+ const [currentcategory,setCurrentcategory]=useState('');
 
 
 
 
 
 const DeleteTask=(e)=> {
-    let indexid=e.target.parentElement.parentElement.parentElement.parentElement.parentElement.id;
-if(indexid) {
+    let deleteid=e.target.parentElement.parentElement.parentElement.parentElement.parentElement.id;
+if(deleteid) {
+    let deleteindex=todolists.findIndex( element => {
+        if (element.id == deleteid) {
+          return true;
+        }
+      });
+
     let deltask=[...todolists];
-    deltask.splice(indexid,1);
+    deltask.splice(deleteindex,1);
   
     setTodolists(deltask);
 }
@@ -25,31 +39,139 @@ if(indexid) {
 
 const MarkUpdate=(e)=> {
 
-let indexid=e.target.parentElement.parentElement.parentElement.parentElement.parentElement.id;
-if(indexid) {
-setShowupdate(true);
-setUtask(todolists[indexid].task);
-setUpdateid(indexid);
+let updateid=e.target.parentElement.parentElement.parentElement.parentElement.parentElement.id;
+if(updateid) {
+    let localupdateindex=todolists.findIndex( element => {
+        if (element.id == updateid) {
+          return true;
+        }
+      });
+
+
+setShowupdatebutton(true);
+setTask(todolists[localupdateindex].task);
+setDescription(todolists[localupdateindex].description);
+setCategory(todolists[localupdateindex].category);
+setUpdateindex(localupdateindex);
+}
 }
 
-}
+const MarkComplete=(e)=> {
+
+    let updateid=e.target.parentElement.parentElement.parentElement.id;
+    if(updateid) {
+        console.log(updateid);
+        let updateindex=todolists.findIndex( element => {
+            if (element.id == updateid) {
+              return true;
+            }
+          });
+        let newtodolists=[...todolists];
+        newtodolists[updateindex].completed=!newtodolists[updateindex].completed;
+    setTodolists(newtodolists);
+    }
+    
+    }
+
 
 const ClearAll=()=> {
     setTodolists([]);
 }
 
 
+
+const Changecolor=(e)=> {
+    let collection=document.getElementsByClassName('filterbutton');
+    
+    for (let x=0;x<collection.length;x++) {
+        collection[x].style.backgroundColor="black";
+        e.target.style.backgroundColor="red";
+      }
+}
+
   return (
     <>
     <div className='todolistscontainer'>
+
+
         {todolists&&<div className='todolists'>
+        <div className='filtercategories'>
+        <div>
+            <button onClick={(e)=>{setFilter("all");Changecolor(e)}} className="filterbutton">All</button ><button onClick={(e)=>{setFilter("completed");Changecolor(e)}} className="filterbutton">Completed</button>
+            <button onClick={(e)=>{setFilter("notcompleted");Changecolor(e)}} className="filterbutton">Not Completed</button>
+        </div>
+
+        <div>
+        {categories.map((category,index)=>(
+            <button key={index} className="filterbutton" onClick={(e)=>{setFilter("categories");Changecolor(e);setCurrentcategory(category)}}>{category}</button>
+        ))}
+
+        </div>
+         </div>
+
+        {filter=="all" &&<div>
             {todolists.map((list,index)=> (
-                <div key={index} id={index}>
-                    <p>{list.task} <span><i onClick={(e)=>MarkUpdate(e)}><FaPenSquare/></i> | <i onClick={(e)=>DeleteTask(e)}><AiTwotoneDelete/></i></span></p><hr/>
+                <div key={list.id} id={list.id}>
+                    <p>{!todolists[index].completed &&<i onClick={(e)=>MarkComplete(e)}><ImCheckboxUnchecked/></i>}{todolists[index].completed &&<i onClick={(e)=>MarkComplete(e)}><ImCheckboxChecked/></i>}<b>{list.task}</b> <span><i onClick={(e)=>MarkUpdate(e)}><FaPenSquare/></i> | <i onClick={(e)=>DeleteTask(e)}><AiTwotoneDelete/></i></span></p>
+                    <i>{list.description}</i>
+                    
+                    <hr/>
 
                 </div>
             ))}
-            
+        </div>  }   
+
+        {filter=="completed" &&<div>
+            {todolists.filter((list,index)=> {
+            if(list.completed==true) {
+            return list
+       }
+         }).map((list,index)=> (
+                <div key={list.id} id={list.id}>
+                    <p><i onClick={(e)=>MarkComplete(e)}><ImCheckboxChecked/></i><b>{list.task}</b> <span><i onClick={(e)=>MarkUpdate(e)}><FaPenSquare/></i> | <i onClick={(e)=>DeleteTask(e)}><AiTwotoneDelete/></i></span></p>
+                    <i>{list.description}</i>
+                    
+                    <hr/>
+
+                </div>
+            ))}
+        </div>  }   
+
+        {filter=="notcompleted" &&<div>
+            {todolists.filter((list,index)=> {
+            if(list.completed==false) {
+            return list
+       }
+         }).map((list,index)=> (
+                <div key={list.id} id={list.id}>
+                    <p><i onClick={(e)=>MarkComplete(e)}><ImCheckboxUnchecked/></i><b>{list.task}</b> <span><i onClick={(e)=>MarkUpdate(e)}><FaPenSquare/></i> | <i onClick={(e)=>DeleteTask(e)}><AiTwotoneDelete/></i></span></p>
+                    <i>{list.description}</i>
+                    
+                    <hr/>
+
+                </div>
+            ))}
+        </div>  }  
+     
+
+        {filter=="categories" &&<div>
+            {todolists.filter((list,index)=> {
+            if(list.category==currentcategory) {
+            return list
+       }
+         }).map((list,index)=> (
+                <div key={list.id} id={list.id}>
+                    <p>{!todolists[index].completed &&<i onClick={(e)=>MarkComplete(e)}><ImCheckboxUnchecked/></i>}{todolists[index].completed &&<i onClick={(e)=>MarkComplete(e)}><ImCheckboxChecked/></i>}<b>{list.task}</b> <span><i onClick={(e)=>MarkUpdate(e)}><FaPenSquare/></i> | <i onClick={(e)=>DeleteTask(e)}><AiTwotoneDelete/></i></span></p>
+                    <i>{list.description}</i>
+                    
+                    <hr/>
+
+                </div>
+            ))}
+        </div>  } 
+
+
+
         </div>}
 
 <div className='clearall'>
--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -1,6 +1,7 @@
 import '../css/todolist.css';
 
 import React, { useState } from 'react';
+import uuid from 'react-uuid';
 
 import Lists from './Lists';
 
@@ -8,9 +9,13 @@ import Lists from './Lists';
 export default function Todolist() {
 const [todolists,setTodolists]=useState([]);
 const [task,setTask]=useState('');
-const [utask,setUtask]=useState('');
-const [showupdate,setShowupdate]=useState(false);
-const [updateid,setUpdateid]=useState('');
+const [description,setDescription]=useState('');
+const [category,setCategory]=useState('');
+const [showupdatebutton,setShowupdatebutton]=useState(false);
+const [updateindex,setUpdateindex]=useState('');
+
+const [categories,setCategories]=useState(["general","coding","office","home"]);
+const [customcategory,setCustomcategory]=useState('');
 
 
 const updateTodoList=(e)=> {
@@ -18,46 +23,89 @@ const updateTodoList=(e)=> {
  
 let newtodolist=[...todolists];
 if(task.length>1) {
-newtodolist.push({task:task,done:false});
+newtodolist.push({id:uuid(),task:task,description,category,completed:false});
 setTodolists(newtodolist);
 
-setTask(' ')
+setTask('')
+setDescription('')
+// setCategory('general')
 
 }else {
   alert("Can not add an Empty List")
 }
 
- 
+
 }
 
 
 const UpdateTask=(e)=> {
   e.preventDefault();
-  setShowupdate(false);
+  setShowupdatebutton(false);
 
-  if(updateid) {
+ 
+
+  if(updateindex>-1) {
+    
   let newtodolists=[...todolists];
-  newtodolists[updateid].task=utask;
+  newtodolists[updateindex].task=task;
+  newtodolists[updateindex].description=description;
+  newtodolists[updateindex].category=category;
   setTodolists(newtodolists);
+  setTask('');
+  setDescription('');
+  // setCategory('');
+  }
+}
+
+const Addcustomcategory=()=> {
+  if(customcategory.length>1) {
+let newcategories=[...categories];
+newcategories.push(customcategory);
+setCategories(newcategories);
+setCustomcategory('')
+  }else {
+    alert("can not add empty category")
   }
 
-
 }
+
+
 
   return (
     <>
     <div className='todolist'>
       <form onSubmit={(e)=>updateTodoList(e)}>
-        {showupdate &&<input type='text' value={utask} onChange={(e)=>setUtask(e.target.value)}/> }
-        {!showupdate &&<input type='text' placeholder='Enter Your Task' value={task} onChange={(e)=>setTask(e.target.value)}/>}
-        {!showupdate &&<button type='submit'>Add Task</button>}
-        {showupdate &&<button onClick={(e)=> UpdateTask(e)}>Update</button>}
+        {/* {showupdate &&<input type='text' value={utask} onChange={(e)=>setUtask(e.target.value)}/> } */}
+        <input type='text' placeholder='Enter Your Task' value={task} onChange={(e)=>setTask(e.target.value)}/>
+        {!showupdatebutton &&<button type='submit'>Add Task</button>}
+        {showupdatebutton &&<button onClick={(e)=> UpdateTask(e)}>Update</button>}
+        <textarea type='text' placeholder='Describe Your Task' value={description} onChange={(e)=>setDescription(e.target.value)}/>
+
       </form>
+
+      <div className='categories'>
+        <select value={category} onChange={(e)=>setCategory(e.target.value)}>
+          {categories.map((category,index)=> (
+        
+          <option key={index} value={category}>{category}</option>
+          
+      ))}
+        </select>
+
+        <input placeholder='Custom Category' value={customcategory} onChange={(e)=>setCustomcategory(e.target.value)}/><button type='submit' onClick={(e)=> Addcustomcategory(e)}>Add</button>
+      </div>
+
+     
     </div>
 
     <div>
-      <Lists todolists={todolists} setTodolists={setTodolists} showupdate={showupdate} setShowupdate={setShowupdate} utask={utask} setUtask={setUtask}
-      updateid={updateid} setUpdateid={setUpdateid}
+      <Lists todolists={todolists} setTodolists={setTodolists} setShowupdatebutton={setShowupdatebutton} 
+      updateindex={updateindex} setUpdateindex={setUpdateindex}
+      task={task} setTask={setTask}
+      setDescription={setDescription}
+      setCategory={setCategory}
+      category={category}
+      categories={categories}
       />
     </div>
     </>
